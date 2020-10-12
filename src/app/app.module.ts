ba16381d9f5d6d5b74f5d67f22b181b636fe00ea
clf94-dev <clf94@hotmail.es>
2020-10-12 09:21:37 +0200

    added header(navbar) component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
