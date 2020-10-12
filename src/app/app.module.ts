036f5db0ac679fc850a673f922beec94ec37cbab
clf94-dev <clf94@hotmail.es>
2020-10-12 10:17:35 +0200

    set routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routing, appRoutingProviders} from './app.routing'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormComponent } from './components/form/form.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +18,17 @@ import { FooterComponent } from './components/footer/footer.component';
     NavbarComponent,
     SliderComponent,
     SideBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormComponent,
+    PaginaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import {ModuleWithProviders} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
+
+import {HomeComponent} from './components/home/home.component'
+import {BlogComponent} from './components/blog/blog.component'
+import {FormComponent} from './components/form/form.component'
+import {PaginaComponent} from './components/pagina/pagina.component'
+
+const appRoutes: Routes =[
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'form', component: FormComponent},
+    {path: 'pagina-test', component: PaginaComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders =  RouterModule.forRoot(appRoutes);
