95bcbf3e55ba03873a31580d392af28ad1e96f25
clf94-dev <clf94@hotmail.es>
2020-10-12 12:26:53 +0200

    redirection example with buttons

--- src/app/components/pagina/pagina.component.ts
+++ src/app/components/pagina/pagina.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
-@Component({
-  selector: 'app-pagina',
-  templateUrl: './pagina.component.html',
-  styleUrls: ['./pagina.component.scss']
-})
+@Component({selector: 'app-pagina', templateUrl: './pagina.component.html', styleUrls: ['./pagina.component.scss']})
 export class PaginaComponent implements OnInit {
-public name: string;
-public lastName: string;
-  constructor(
-    private _route: ActivatedRoute,
-    private _router: Router
-  ) { 
-  
-  }
+    public name : string;
+    public lastName : string;
+    constructor(private _route : ActivatedRoute, private _router : Router) {}
 
-  ngOnInit(): void {
+    ngOnInit() : void {
 
-    this._route.params.subscribe((params: Params) =>
-    {
-console.log(params);
-this.name= params.nombre;
-this.lastName= params.apellido;
-    });
-  }
+        this
+            ._route
+            .params
+            .subscribe((params : Params) => {
+                console.log(params);
+                this.name = params.nombre;
+                this.lastName = params.apellido;
+            });
+    }
+
+    redireccionHome(){
+      this._router.navigate(['/home']);
+    }
+    redireccionBlog(){
+      this._router.navigate(['/blog']);
+    }
+    redireccionForm(){
+      this._router.navigate(['/form']);
+    }
 
 }
